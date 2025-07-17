1d8921cda882c640a1714979c66d95fcc0666bce
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-17 21:19:32 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // src/App.jsx
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ConnectWallet from "./components/ConnectWallet";
 import GameArena from "./components/GameArena";
@@ -6,13 +7,24 @@ import ArenaPVP from "./components/ArenaPVP";
 import ArenaPVE from "./components/ArenaPVE";
 
 function App() {
+  const [signer, setSigner] = useState(null);
+  const [walletAddress, setWalletAddress] = useState("");
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ConnectWallet />} />
-        <Route path="/arena" element={<GameArena />} />
-        <Route path="/arena-pvp" element={<ArenaPVP />} />
-        <Route path="/arena-pve" element={<ArenaPVE />} />
+        <Route
+          path="/"
+          element={
+            <ConnectWallet
+              setSigner={setSigner}
+              setWalletAddress={setWalletAddress}
+            />
+          }
+        />
+        <Route path="/arena" element={<GameArena signer={signer} walletAddress={walletAddress} />} />
+        <Route path="/arena-pvp" element={<ArenaPVP signer={signer} walletAddress={walletAddress} />} />
+        <Route path="/arena-pve" element={<ArenaPVE signer={signer} walletAddress={walletAddress} />} />
       </Routes>
     </Router>
   );
