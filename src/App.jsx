56a11679a9abefb9c1116681525a1c8913191e77
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-17 21:30:01 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 // src/App.jsx
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import ConnectWallet from "./components/ConnectWallet";
 import GameArena from "./components/GameArena";
 import ArenaPVP from "./components/ArenaPVP";
 import ArenaPVE from "./components/ArenaPVE";
 
 function App() {
-  const [signer, setSigner] = useState(null);
-  const [walletAddress, setWalletAddress] = useState("");
+  const [walletConnected, setWalletConnected] = useState(false);
 
   return (
     <Router>
@@ -16,15 +15,12 @@ function App() {
         <Route
           path="/"
           element={
-            <ConnectWallet
-              setSigner={setSigner}
-              setWalletAddress={setWalletAddress}
-            />
+            <ConnectWallet setWalletConnected={setWalletConnected} />
           }
         />
-        <Route path="/arena" element={<GameArena signer={signer} walletAddress={walletAddress} />} />
-        <Route path="/arena-pvp" element={<ArenaPVP signer={signer} walletAddress={walletAddress} />} />
-        <Route path="/arena-pve" element={<ArenaPVE signer={signer} walletAddress={walletAddress} />} />
+        <Route path="/arena" element={<GameArena />} />
+        <Route path="/arena-pvp" element={<ArenaPVP />} />
+        <Route path="/arena-pve" element={<ArenaPVE />} />
       </Routes>
     </Router>
   );
