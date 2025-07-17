672c809fdf9e3816a2405e540ede05bcec4ca631
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-17 18:41:21 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import GameArena from './components/GameArena';
+export default function App() {
+  return (<div className='min-h-screen bg-gray-900 text-white'><GameArena /></div>);
+}
