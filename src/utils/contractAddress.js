6674d90a516f93ef60d1da15514f3c959d695114
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-17 20:24:11 +0700

    Update contractAddress.js

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -1,7 +1,3 @@
 // src/utils/contractAddress.js
 
-// Alamat smart contract ArenaDuelTurnBased di jaringan testnet Somnia
-// Ganti alamat ini jika kamu deploy ulang kontraknya
-const contractAddress = "0x95dd66c55214a3d603fe1657e22f710692fcbd9b";
-
-export default contractAddress;
+export const CONTRACT_ADDRESS = "0x95dd66c55214a3d603fe1657e22f710692fcbd9b";
