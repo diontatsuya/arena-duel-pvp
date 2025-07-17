b7d916b6a6a3641f53d80a93d1703a80dd43cb8e
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-17 14:25:46 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { PrivyProvider } from '@privy-io/react-auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PrivyProvider
+      appId="cmcn6y46j00mnl40m3u5bee9v"
+      config={{
+        loginMethods: ['wallet'],
+        appearance: { theme: 'light' }
+      }}
+    >
+      <App />
+    </PrivyProvider>
   </React.StrictMode>
 );
