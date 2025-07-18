efbb2a4347310d8227c2ec6a84198a84b34b36bf
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-18 12:06:23 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ConnectWallet from "./components/ConnectWallet";
 import GameArena from "./components/GameArena";
 import ArenaPVP from "./components/ArenaPVP";
 import ArenaPVE from "./components/ArenaPVE";
+import ArenaUI from "./components/ArenaUI"; // Tambahan untuk mode debug
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/arena" element={<GameArena />} />
         <Route path="/arena-pvp" element={<ArenaPVP />} />
         <Route path="/arena-pve" element={<ArenaPVE />} />
+        <Route path="/debug" element={<ArenaUI />} /> {/* Halaman debug */}
       </Routes>
     </Router>
   );
