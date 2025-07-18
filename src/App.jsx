a009e61a24e89ca1af9307a40b32f9c7d0b436df
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-18 11:37:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ConnectWallet from "./components/ConnectWallet";
 import GameArena from "./components/GameArena";
 import ArenaPVP from "./components/ArenaPVP";
 import ArenaPVE from "./components/ArenaPVE";
-import ArenaUI from "./components/ArenaUI"; // Import ArenaUI
 
 function App() {
   return (
@@ -14,7 +12,6 @@ function App() {
         <Route path="/arena" element={<GameArena />} />
         <Route path="/arena-pvp" element={<ArenaPVP />} />
         <Route path="/arena-pve" element={<ArenaPVE />} />
-        <Route path="/debug" element={<ArenaUI />} /> {/* Route debug */}
       </Routes>
     </Router>
   );
