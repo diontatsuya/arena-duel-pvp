e555e6cb5297c5e46f0dab1e309cfe67ad24f373
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-20 02:47:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/ui/Navbar";
 import Home from "./pages/Home";
 import ArenaPVP from "./pages/ArenaPVP";
 import ArenaPVE from "./pages/ArenaPVE";
