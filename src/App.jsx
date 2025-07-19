4345be71db3f9344fc45743ae3904607c34ea38b
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-20 01:04:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,82 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import ArenaPVE from "./pages/ArenaPVE";
 import ArenaPVP from "./pages/ArenaPVP";
+import ArenaPVE from "./pages/ArenaPVE";
+import Home from "./pages/Home";
+import "./App.css";
 
-const App = () => {
+const pageVariants = {
+  initial: { opacity: 0, y: 30 },
+  in: { opacity: 1, y: 0 },
+  out: { opacity: 0, y: -30 },
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "easeInOut",
+  duration: 0.4,
+};
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
   return (
-    <Router>
-      <div className="min-h-screen bg-black text-white">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pve" element={<ArenaPVE />} />
-          <Route path="/pvp" element={<ArenaPVP />} />
-        </Routes>
-      </div>
-    </Router>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              initial="initial"
+              animate="in"
+              exit="out"
+              variants={pageVariants}
+              transition={pageTransition}
+            >
+              <Home />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/pvp"
+          element={
+            <motion.div
+              initial="initial"
+              animate="in"
+              exit="out"
+              variants={pageVariants}
+              transition={pageTransition}
+            >
+              <ArenaPVP />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/pve"
+          element={
+            <motion.div
+              initial="initial"
+              animate="in"
+              exit="out"
+              variants={pageVariants}
+              transition={pageTransition}
+            >
+              <ArenaPVE />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
   );
 };
+
+function App() {
+  return (
+    <Router>
+      <AnimatedRoutes />
+    </Router>
+  );
+}
 
 export default App;
