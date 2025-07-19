68fa9f9704eb46b33b069faf98cc7e28df3802b0
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-20 04:01:54 +0700

    Update ArenaPVP.jsx

--- src/pages/ArenaPVP.jsx
+++ src/pages/ArenaPVP.jsx
@@ -1,7 +1,7 @@
 // src/pages/ArenaPVP.jsx
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { connectWallet } from "../utils/connectWallet";
+import { connectWallet } from "../utils/connectwallet";
 import { contractABI } from "../utils/contractABI";
 import { CONTRACT_ADDRESS } from "../utils/constants";
 
