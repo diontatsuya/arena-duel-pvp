ea748b485254022506d3b5031244f19749a3b238
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-20 11:39:00 +0700

    Delete src/lib/game directory

--- src/lib/game/pveLogic.js
+++
@@ -1,8 +0,0 @@
-import { resolveTurn } from "./gameEngine";
-import { getRandomAction } from "./utils";
-
-export async function handlePveTurn(playerAction, playerHP, aiHP) {
-  const aiAction = getRandomAction();
-  const result = resolveTurn(playerAction, aiAction, playerHP, aiHP);
-  return { ...result, aiAction };
-}
