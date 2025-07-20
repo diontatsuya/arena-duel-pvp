fd548024c5f7b44aeeaa829bb1526841313f49dd
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-20 15:15:28 +0700

    Update PvPManager.js

--- src/gameLogic/pvp/PvPManager.js
+++ src/gameLogic/pvp/PvPManager.js
@@ -1,8 +1,32 @@
 // src/gameLogic/pvp/PvPManager.js
-export const getOpponent = (players, currentPlayer) => {
-  return players.find(p => p.address !== currentPlayer.address);
+
+import { ethers } from "ethers";
+import { contractABI } from "../../utils/contractABI";
+import { CONTRACT_ADDRESS } from "../../utils/constants";
+
+// Mendapatkan status game PVP untuk wallet tertentu
+export const getPVPGameState = async (contract, walletAddress) => {
+  const player = await contract.players(walletAddress);
+  const opponent = await contract.players(player.opponent);
+
+  return {
+    player: {
+      address: walletAddress,
+      hp: player.hp.toNumber(),
+      lastAction: player.lastAction,
+      isTurn: player.isTurn,
+    },
+    opponent: {
+      address: player.opponent,
+      hp: opponent.hp.toNumber(),
+      lastAction: opponent.lastAction,
+      isTurn: opponent.isTurn,
+    },
+  };
 };
 
-export const isPlayerTurn = (turnAddress, playerAddress) => {
-  return turnAddress === playerAddress;
+// Menangani aksi: Attack, Defend, Heal
+export const handlePVPAction = async (contract, actionType) => {
+  const tx = await contract.takeAction(actionType);
+  await tx.wait();
 };
