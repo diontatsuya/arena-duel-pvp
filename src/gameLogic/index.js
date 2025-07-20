6b551b635d2a4513ca0cc322992e955936187a06
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-20 13:23:23 +0700

    Update index.js

--- src/gameLogic/index.js
+++ src/gameLogic/index.js
@@ -3,4 +3,7 @@ export * from "./engine/Actions";
 export * from "./engine/BattleEngine";
 export * from "./engine/GameUtils";
 export * from "./pvp/PvPManager";
-export * from "./pve/AIOpponent";
+export * from "./pve/AIOpponents"; // sebelumnya typo: AIOpponent → AIOpponents
+
+export { GameState } from "./GameState";
+export { TurnManager } from "./TurnManager";
