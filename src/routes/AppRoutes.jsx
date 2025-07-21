2f4fdc56daf19e1a04321a27746a3c529b2b0708
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-21 12:29:32 +0700

    Create AppRoutes.jsx

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+// src/routes/AppRoutes.jsx
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import ArenaPVP from "../pages/ArenaPVP";
+import ArenaPVE from "../pages/ArenaPVE";
+import NotFound from "../pages/NotFound";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/arena-pvp" element={<ArenaPVP />} />
+      <Route path="/arena-pve" element={<ArenaPVE />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
