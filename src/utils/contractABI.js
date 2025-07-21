20055625f433dd7782dc3c91d4bbcc33f7b71e99
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-21 12:20:29 +0700

    Update contractABI.js

--- src/utils/contractABI.js
+++ src/utils/contractABI.js
@@ -1,50 +1,14 @@
-// src/utils/contractABI.js
-
-export const contractAddress = "0x95dd66c55214a3d603fe1657e22f710692fcbd9b";
-
 export const contractABI = [
   {
     "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      { "indexed": true, "internalType": "address", "name": "player", "type": "address" },
-      { "indexed": true, "internalType": "address", "name": "opponent", "type": "address" }
-    ],
-    "name": "Matched",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      { "indexed": true, "internalType": "address", "name": "player", "type": "address" },
-      { "indexed": false, "internalType": "enum ArenaDuelTurnBased.Action", "name": "action", "type": "uint8" },
-      { "indexed": false, "internalType": "uint256", "name": "playerHp", "type": "uint256" },
-      { "indexed": false, "internalType": "uint256", "name": "opponentHp", "type": "uint256" }
-    ],
-    "name": "ActionTaken",
-    "type": "event"
-  },
-  {
-    "inputs": [],
-    "name": "waitingPlayer",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "joinGame",
+    "name": "joinArena",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
   },
   {
     "inputs": [{ "internalType": "uint8", "name": "action", "type": "uint8" }],
-    "name": "takeAction",
+    "name": "performAction",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
@@ -56,9 +20,34 @@ export const contractABI = [
       { "internalType": "address", "name": "opponent", "type": "address" },
       { "internalType": "uint256", "name": "hp", "type": "uint256" },
       { "internalType": "bool", "name": "isTurn", "type": "bool" },
-      { "internalType": "enum ArenaDuelTurnBased.Action", "name": "lastAction", "type": "uint8" }
+      { "internalType": "uint8", "name": "lastAction", "type": "uint8" }
     ],
     "stateMutability": "view",
     "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "waitingPlayer",
+    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "address", "name": "player1", "type": "address" },
+      { "indexed": false, "internalType": "address", "name": "player2", "type": "address" }
+    ],
+    "name": "Matched",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "address", "name": "player", "type": "address" },
+      { "indexed": false, "internalType": "uint8", "name": "action", "type": "uint8" }
+    ],
+    "name": "ActionPerformed",
+    "type": "event"
   }
 ];
