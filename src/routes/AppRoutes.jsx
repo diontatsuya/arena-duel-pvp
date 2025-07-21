98f1b6db91ae32f936d71e33279037fbde81ed77
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-21 19:26:25 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ArenaPVP from "../pages/ArenaPVP";
 import ArenaPVE from "../pages/ArenaPVE";
-import GamePVP from "../pages/GamePVP";
+import GamePVP from "../gameLogic/GamePVP";
+import GamePVE from "../gameLogic/GamePVE";
 import NotFound from "../pages/NotFound";
 
 const AppRoutes = () => {
@@ -12,7 +13,8 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/arena-pvp" element={<ArenaPVP />} />
       <Route path="/arena-pve" element={<ArenaPVE />} />
-      <Route path="/game-pvp" element={<GamePVP />} />
+      <Route path="/game/pvp" element={<GamePVP />} />
+      <Route path="/game/pve" element={<GamePVE />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
