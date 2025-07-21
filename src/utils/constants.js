e15498a267d72155d717f3653add036edf5f36fd
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-21 12:19:20 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,1 @@
-// src/utils/constants.js
-
-// Alamat kontrak ArenaDuelTurnBased kamu (ganti dengan yang kamu gunakan di Somnia testnet)
 export const CONTRACT_ADDRESS = "0x95dd66c55214a3d603fe1657e22f710692fcbd9b";
-
-// Nilai default HP
-export const INITIAL_HP = 100;
-export const MAX_HP = 100;
-
-// Aksi-aksi yang tersedia dalam permainan
-export const ACTIONS = ["attack", "defend", "heal"];
