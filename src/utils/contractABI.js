591c665125bd8eca6cd3a51e95d07addb9c45264
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-23 01:34:16 +0700

    Update contractABI.js

--- src/utils/contractABI.js
+++ src/utils/contractABI.js
@@ -1,35 +1,31 @@
 export const contractABI = [
   {
-    "inputs": [],
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "address", "name": "player", "type": "address" },
+      { "indexed": false, "internalType": "enum ArenaDuelTurnBased.Action", "name": "action", "type": "uint8" },
+      { "indexed": false, "internalType": "uint256", "name": "playerHp", "type": "uint256" },
+      { "indexed": false, "internalType": "uint256", "name": "opponentHp", "type": "uint256" }
+    ],
+    "name": "ActionTaken",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": false, "internalType": "address", "name": "winner", "type": "address" },
+      { "indexed": false, "internalType": "address", "name": "loser", "type": "address" }
+    ],
+    "name": "GameOver",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      { "internalType": "enum ArenaDuelTurnBased.GameMode", "name": "mode", "type": "uint8" }
+    ],
     "name": "joinArena",
     "outputs": [],
     "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "uint8", "name": "action", "type": "uint8" }],
-    "name": "performAction",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "name": "players",
-    "outputs": [
-      { "internalType": "address", "name": "opponent", "type": "address" },
-      { "internalType": "uint256", "name": "hp", "type": "uint256" },
-      { "internalType": "bool", "name": "isTurn", "type": "bool" },
-      { "internalType": "uint8", "name": "lastAction", "type": "uint8" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "waitingPlayer",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
     "type": "function"
   },
   {
@@ -42,12 +38,49 @@ export const contractABI = [
     "type": "event"
   },
   {
-    "anonymous": false,
     "inputs": [
-      { "indexed": false, "internalType": "address", "name": "player", "type": "address" },
-      { "indexed": false, "internalType": "uint8", "name": "action", "type": "uint8" }
+      { "internalType": "enum ArenaDuelTurnBased.Action", "name": "action", "type": "uint8" }
     ],
-    "name": "ActionPerformed",
-    "type": "event"
+    "name": "takeAction",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "player", "type": "address" }
+    ],
+    "name": "getStatus",
+    "outputs": [
+      { "internalType": "address", "name": "opponent", "type": "address" },
+      { "internalType": "uint256", "name": "hp", "type": "uint256" },
+      { "internalType": "bool", "name": "isTurn", "type": "bool" },
+      { "internalType": "enum ArenaDuelTurnBased.Action", "name": "lastAction", "type": "uint8" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "", "type": "address" }
+    ],
+    "name": "players",
+    "outputs": [
+      { "internalType": "address", "name": "opponent", "type": "address" },
+      { "internalType": "uint256", "name": "hp", "type": "uint256" },
+      { "internalType": "bool", "name": "isTurn", "type": "bool" },
+      { "internalType": "enum ArenaDuelTurnBased.Action", "name": "lastAction", "type": "uint8" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "waitingPlayer",
+    "outputs": [
+      { "internalType": "address", "name": "", "type": "address" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   }
 ];
