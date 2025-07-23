99f3dd0228c12cee15754554f1f03217c1fb88ae
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-23 18:13:28 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ArenaPVP from "../pages/ArenaPVP";
 import ArenaPVE from "../pages/ArenaPVE";
+import PVPJoin from "../pages/PVPJoin";
 import NotFound from "../pages/NotFound"; // Pastikan file ini ada
 
 const AppRoutes = () => {
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/pvp" element={<ArenaPVP />} />
       <Route path="/pve" element={<ArenaPVE />} />
+      <Route path="/join-pvp" element={<PVPJoin />} /> {/* ✅ route baru */}
       <Route path="*" element={<NotFound />} /> {/* Tambahkan ini */}
     </Routes>
   );
