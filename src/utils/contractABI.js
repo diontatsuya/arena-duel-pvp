0170a2614b25b9ef860fcf202df64bb3a5ff1979
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-27 20:22:27 +0700

    Update contractABI.js

--- src/utils/contractABI.js
+++ src/utils/contractABI.js
@@ -1,80 +1,199 @@
 export const contractABI = [
-  {
-    "inputs": [],
-    "name": "joinGame",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "uint8", "name": "_action", "type": "uint8" }
-    ],
-    "name": "performAction",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "_player", "type": "address" }
-    ],
-    "name": "getPlayerStatus",
-    "outputs": [
-      {
-        "components": [
-          { "internalType": "address", "name": "opponent", "type": "address" },
-          { "internalType": "uint256", "name": "hp", "type": "uint256" },
-          { "internalType": "bool", "name": "isTurn", "type": "bool" },
-          { "internalType": "uint8", "name": "lastAction", "type": "uint8" }
-        ],
-        "internalType": "struct ArenaDuelTurnBased.Player",
-        "name": "",
-        "type": "tuple"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "name": "players",
-    "outputs": [
-      { "internalType": "address", "name": "opponent", "type": "address" },
-      { "internalType": "uint256", "name": "hp", "type": "uint256" },
-      { "internalType": "bool", "name": "isTurn", "type": "bool" },
-      { "internalType": "uint8", "name": "lastAction", "type": "uint8" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "waitingPlayer",
-    "outputs": [
-      { "internalType": "address", "name": "", "type": "address" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      { "indexed": true, "internalType": "address", "name": "player", "type": "address" },
-      { "indexed": false, "internalType": "uint8", "name": "action", "type": "uint8" },
-      { "indexed": false, "internalType": "uint256", "name": "damage", "type": "uint256" },
-      { "indexed": false, "internalType": "uint256", "name": "heal", "type": "uint256" }
-    ],
-    "name": "ActionTaken",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      { "indexed": true, "internalType": "address", "name": "player1", "type": "address" },
-      { "indexed": true, "internalType": "address", "name": "player2", "type": "address" }
-    ],
-    "name": "Matched",
-    "type": "event"
-  }
+[
+{
+"anonymous": false,
+"inputs": [
+{
+"indexed": true,
+"internalType": "address",
+"name": "player",
+"type": "address"
+},
+{
+"indexed": false,
+"internalType": "enum ArenaDuelTurnBased.Action",
+"name": "action",
+"type": "uint8"
+},
+{
+"indexed": false,
+"internalType": "uint256",
+"name": "playerHp",
+"type": "uint256"
+},
+{
+"indexed": false,
+"internalType": "uint256",
+"name": "opponentHp",
+"type": "uint256"
+}
+],
+"name": "ActionTaken",
+"type": "event"
+},
+{
+"anonymous": false,
+"inputs": [
+{
+"indexed": true,
+"internalType": "address",
+"name": "winner",
+"type": "address"
+},
+{
+"indexed": true,
+"internalType": "address",
+"name": "loser",
+"type": "address"
+}
+],
+"name": "GameOver",
+"type": "event"
+},
+{
+"anonymous": false,
+"inputs": [
+{
+"indexed": true,
+"internalType": "address",
+"name": "player",
+"type": "address"
+}
+],
+"name": "GameReset",
+"type": "event"
+},
+{
+"inputs": [],
+"name": "joinGame",
+"outputs": [],
+"stateMutability": "nonpayable",
+"type": "function"
+},
+{
+"anonymous": false,
+"inputs": [
+{
+"indexed": true,
+"internalType": "address",
+"name": "player1",
+"type": "address"
+},
+{
+"indexed": true,
+"internalType": "address",
+"name": "player2",
+"type": "address"
+}
+],
+"name": "Matched",
+"type": "event"
+},
+{
+"inputs": [],
+"name": "resetGame",
+"outputs": [],
+"stateMutability": "nonpayable",
+"type": "function"
+},
+{
+"inputs": [
+{
+"internalType": "enum ArenaDuelTurnBased.Action",
+"name": "action",
+"type": "uint8"
+}
+],
+"name": "takeAction",
+"outputs": [],
+"stateMutability": "nonpayable",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "getMyStatus",
+"outputs": [
+{
+"internalType": "address",
+"name": "opponent",
+"type": "address"
+},
+{
+"internalType": "uint256",
+"name": "myHp",
+"type": "uint256"
+},
+{
+"internalType": "uint256",
+"name": "opponentHp",
+"type": "uint256"
+},
+{
+"internalType": "bool",
+"name": "isMyTurn",
+"type": "bool"
+},
+{
+"internalType": "enum ArenaDuelTurnBased.Action",
+"name": "myLastAction",
+"type": "uint8"
+},
+{
+"internalType": "enum ArenaDuelTurnBased.Action",
+"name": "opponentLastAction",
+"type": "uint8"
+}
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [
+{
+"internalType": "address",
+"name": "",
+"type": "address"
+}
+],
+"name": "players",
+"outputs": [
+{
+"internalType": "address",
+"name": "opponent",
+"type": "address"
+},
+{
+"internalType": "uint256",
+"name": "hp",
+"type": "uint256"
+},
+{
+"internalType": "bool",
+"name": "isTurn",
+"type": "bool"
+},
+{
+"internalType": "enum ArenaDuelTurnBased.Action",
+"name": "lastAction",
+"type": "uint8"
+}
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "waitingPlayer",
+"outputs": [
+{
+"internalType": "address",
+"name": "",
+"type": "address"
+}
+],
+"stateMutability": "view",
+"type": "function"
+}
+]
+
 ];
