a4e3eaff8bd8bd0d9a508c5991d1ecd9c23e2c12
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-29 22:42:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/ui/Navbar";
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-900 text-white">
-        <Navbar />
-        <AppRoutes />
-      </div>
-    </Router>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
