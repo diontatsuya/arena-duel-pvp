40a857e715e46bdb2792d97a8f9757196b192474
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-29 12:44:23 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ArenaPVP from "./pages/ArenaPVP";
-import JoinPVP from "./pages/JoinPVP";
-import ArenaPVE from "./pages/ArenaPVE";
-import NotFound from "./pages/NotFound";
+import ArenaPVP from "../pages/ArenaPVP";
+import JoinPVP from "../pages/JoinPVP";
+import ArenaPVE from "../pages/ArenaPVE";
+import NotFound from "../pages/NotFound";
 
 const App = () => {
   return (
