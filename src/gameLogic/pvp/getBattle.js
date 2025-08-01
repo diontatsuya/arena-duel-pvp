eec2aeb521690dde384d1a8d4021dff59d85bacd
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-02 01:55:41 +0700

    Update getBattle.js

--- src/gameLogic/pvp/getBattle.js
+++ src/gameLogic/pvp/getBattle.js
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { contractABI } from "../../utils/constants/contractABI";
+import { contractABI } from "../../utils/contractABI";
 import { CONTRACT_ADDRESS } from "../../utils/constants";
 
 export const getBattle = async (provider, battleId) => {
