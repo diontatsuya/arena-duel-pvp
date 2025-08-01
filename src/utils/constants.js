737e7379c3db189e356adfdfca51cfaa2da96cfc
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-01 12:48:03 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const CONTRACT_ADDRESS = "0x03892903e86e6db9bbcc86bdff571ca1360184b7"
+export const CONTRACT_ADDRESS = "0x03892903e86e6db9bbcc86bdff571ca1360184b7";
+export const SOMNIA_CHAIN_ID = "0xc488"; // 50312 (hex)
