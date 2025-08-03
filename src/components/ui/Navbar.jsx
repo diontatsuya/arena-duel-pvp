d09383d8dc1cd870eaaca5dfededbbcb6a9a33fc
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-03 20:13:58 +0700

    Update Navbar.jsx

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { WalletContext } from "../contexts/WalletContext";
+import { WalletContext } from "../context/WalletContext";
 
 const Navbar = () => {
   const {
