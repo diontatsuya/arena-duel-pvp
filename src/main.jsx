43c59ebd621e895c26e770c2e9fae97a4bba42ef
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-03 19:54:25 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./router/AppRoutes"; // ganti App menjadi AppRoutes
+import { WalletProvider } from "./context/WalletContext"; // pastikan path-nya benar
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <WalletProvider>
+      <AppRoutes />
+    </WalletProvider>
   </React.StrictMode>
 );
