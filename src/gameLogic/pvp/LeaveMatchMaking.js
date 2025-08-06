72975972c9513a36100bc668312f3eb0579a2918
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-06 15:24:50 +0700

    Update LeaveMatchMaking.js

--- src/gameLogic/pvp/LeaveMatchMaking.js
+++ src/gameLogic/pvp/LeaveMatchMaking.js
@@ -1,17 +1,23 @@
+import { useContext } from "react";
 import { ethers } from "ethers";
 import { CONTRACT_ADDRESS } from "../../utils/constants";
 import { contractABI } from "../../utils/contractABI";
-import { getSignerFromContext } from "../../utils/getSignerFromContext";
+import { WalletContext } from "../../context/WalletContext";
 
-export const leaveMatchmaking = async () => {
-  try {
-    const signer = getSignerFromContext();
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
-    const tx = await contract.leaveMatchmaking();
-    await tx.wait();
-    return true;
-  } catch (error) {
-    console.error("Gagal leave matchmaking:", error);
-    return false;
-  }
+export const useLeaveMatchmaking = () => {
+  const { signer } = useContext(WalletContext);
+
+  const leaveMatchmaking = async () => {
+    try {
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+      const tx = await contract.leaveMatchmaking();
+      await tx.wait();
+      return true;
+    } catch (error) {
+      console.error("Gagal leave matchmaking:", error);
+      return false;
+    }
+  };
+
+  return { leaveMatchmaking };
 };
