32a519633ecc6933855a2c189a712491eae00e4e
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-06 12:34:57 +0700

    Update getBattle.js

--- src/gameLogic/pvp/getBattle.js
+++ src/gameLogic/pvp/getBattle.js
@@ -5,22 +5,25 @@ import { CONTRACT_ADDRESS } from "../../utils/constants";
 export const getBattle = async (provider, battleId) => {
   try {
     const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, provider);
-    const battle = await contract.battles(battleId);
+    const battle = await contract.getBattle(battleId);
 
     return {
       battleId: battleId.toString(),
       player1: {
         address: battle.player1.addr,
-        hp: battle.player1.hp.toNumber(),
-        lastAction: battle.player1.lastAction, // uint8 (0: attack, 1: defend, 2: heal)
+        hp: Number(battle.player1.hp),
+        lastAction: Number(battle.player1.lastAction),
+        defending: battle.player1.defending,
       },
       player2: {
         address: battle.player2.addr,
-        hp: battle.player2.hp.toNumber(),
-        lastAction: battle.player2.lastAction,
+        hp: Number(battle.player2.hp),
+        lastAction: Number(battle.player2.lastAction),
+        defending: battle.player2.defending,
       },
-      turn: battle.currentTurn, // address
-      state: battle.state, // uint8 enum
+      turn: battle.currentTurn,
+      state: Number(battle.state),
+      winner: battle.winner,
     };
   } catch (err) {
     console.error("Gagal mendapatkan data battle:", err);
