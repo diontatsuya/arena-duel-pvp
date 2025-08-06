edba6d5edf4e3504ec848a27a993bf14b395bc8e
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-06 15:15:30 +0700

    Update LeaveMatchMaking.js

--- src/gameLogic/pvp/LeaveMatchMaking.js
+++ src/gameLogic/pvp/LeaveMatchMaking.js
@@ -1,9 +1,11 @@
 import { ethers } from "ethers";
 import { CONTRACT_ADDRESS } from "../../utils/constants";
 import { contractABI } from "../../utils/contractABI";
+import { getSignerFromContext } from "../../utils/getSignerFromContext";
 
-export const leaveMatchmaking = async (signer) => {
+export const leaveMatchmaking = async () => {
   try {
+    const signer = getSignerFromContext();
     const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
     const tx = await contract.leaveMatchmaking();
     await tx.wait();
