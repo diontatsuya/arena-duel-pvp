313e99fe9ca742fd94abaaaa6a4d11eddc9a30b1
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-08 15:58:00 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./routes/AppRoutes"; // ganti App menjadi AppRoutes
-import { WalletProvider } from "./context/WalletContext"; // pastikan path-nya benar
+import AppRoutes from "./routes/AppRoutes";
+import { WalletProvider } from "./context/WalletContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
