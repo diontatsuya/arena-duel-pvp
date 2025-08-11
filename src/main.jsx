4b8e932eeb682a8d8820956dc2347b30c142ae60
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-11 19:11:17 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { WalletProvider } from "./context/WalletContext";
 import "./index.css";
@@ -7,7 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <WalletProvider>
-      <AppRoutes />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </WalletProvider>
   </React.StrictMode>
 );
