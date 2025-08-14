38282feeee50f08dedbf049a4580686a0dfda964
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-14 16:40:35 +0700

    Delete src/gameLogic/index.js

--- src/gameLogic/index.js
+++
@@ -1,9 +0,0 @@
-// src/gameLogic/index.js
-export * from "./engine/Actions";
-export * from "./engine/BattleEngine";
-export * from "./engine/GameUtils";
-export * from "./pvp/PvPManager";
-export * from "./pve/AIOpponents"; // sebelumnya typo: AIOpponent → AIOpponents
-
-export { GameState } from "./GameState";
-export { TurnManager } from "./TurnManager";
