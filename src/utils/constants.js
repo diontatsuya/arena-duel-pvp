e37b33e56506962869dd95ea7a8bc8156494cba7
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-14 17:37:07 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const CONTRACT_ADDRESS = "0xdf5604348b2ba04dda19f9dfc6f9e9376b248538";
-export const CHAIN_ID = "0xc488"; // 50312 (hex)
+export const CHAIN_ID = 50312;
 export const ACTION = {
   ATTACK: 0,
-  DEFFEND: 1,
+  DEFEND: 1,
   HEAL: 2,
 };
