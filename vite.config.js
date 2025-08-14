f1bd08397c745b9d0b8797331dde0cd664279cb0
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-08-14 16:14:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./", // ganti dari "/" ke "./" agar tidak blank saat deploy
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects",
-          dest: ".", // tetap benar
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
 });
